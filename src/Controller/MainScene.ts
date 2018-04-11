d1945a921df0f93ddad9f1c10fdbd1edc31ee7f0
Fractal Games <antonov.martin@gmail.com>
2018-04-11 18:01:22 +0300

    Added resizing

--- src/Controller/MainScene.ts
+++ src/Controller/MainScene.ts
@@ -8,33 +8,55 @@ module GTE {
     /**A class for the main part of the software. This is the starting point of the core software*/
     export class MainScene extends Phaser.State {
         // The Tree Controller handles everything related to the tree
-       treeController:TreeController;
+        treeController: TreeController;
         // User Action Controller handles actions from the user. These actions will be called whenever a keyboard key
         // or a button is pressed. Abstracts the logic of user actions and removes unnecessary code repetition.
-        userActionController:UserActionController;
+        userActionController: UserActionController;
         // The Keyboard Controller handles input and sends signals and executes methods from the treeController
-        keyboardController:KeyboardController;
+        keyboardController: KeyboardController;
         //The Hover Menu Manager handles the buttons and signals when hovering on a node or iset.
-        hoverManager:HoverMenuController;
+        hoverManager: HoverMenuController;
         // Top Menu with action buttons
-        topMenu:TopMenu;
+        topMenu: TopMenu;
         //The label input field which appears when you click on a label
-        labelInput:LabelInput;
+        labelInput: LabelInput;
+        // For resizing the game
+        resizeLocked: boolean;
 
         /** The create method is built-into the engine for every state. It acts as a constructor.*/
         create() {
             this.labelInput = new LabelInput(this.game);
             this.treeController = new TreeController(this.game, this.labelInput);
-            this.userActionController = new UserActionController(this.game,this.treeController);
+            this.userActionController = new UserActionController(this.game, this.treeController);
             this.keyboardController = new KeyboardController(this.game, this.userActionController);
             this.hoverManager = new HoverMenuController(this.game, this.userActionController);
             this.topMenu = new TopMenu(this.userActionController);
+
+            this.resizeLocked = false;
+            window.onresize = () => {
+                if (!this.resizeLocked) {
+                    this.resizeLocked = true;
+                    this.game.time.events.add(100, () => {
+                        let width = window.innerWidth * devicePixelRatio;
+                        let height = window.innerHeight * devicePixelRatio;
+                        if (width > 1920) {
+                            width = 1920;
+                            height = 1920 / window.innerWidth * window.innerHeight;
+                        }
+                        this.game.scale.setGameSize(width, height);
+                        this.treeController.treeViewProperties = new TreeViewProperties(this.game.height * INITIAL_TREE_HEIGHT, this.game.width * INITIAL_TREE_WIDTH);
+                        this.treeController.treeView.properties = this.treeController.treeViewProperties;
+                        this.treeController.resetTree(true, false);
+                        this.resizeLocked = false;
+                    });
+                }
+            }
         }
 
         /** The update method is built-into the engine for every state. It executes at most 60 times a second*/
-        update(){
-          this.treeController.update();
-          this.userActionController.updateCutSpriteHandler();
+        update() {
+            this.treeController.update();
+            this.userActionController.updateCutSpriteHandler();
         }
 
         /** This is used for testing purposes - displays a text 60 times a second in the app*/
@@ -43,7 +65,7 @@ module GTE {
             // this.game.debug.text(this.game.world.children.length.toString(), 20,20, "#ff0000", "20px Arial");
             // if(this.treeController) {
             //     this.game.debug.text("undoTrees: "+this.treeController.undoRedoController.treesList.length,20,80,"#000000","20px Arial");
-                // this.game.debug.text("index: "+this.treeController.undoRedoController.currentTreeIndex,20,100,"#000000","20px Arial");
+            // this.game.debug.text("index: "+this.treeController.undoRedoController.currentTreeIndex,20,100,"#000000","20px Arial");
             // }
             // this.game.debug.text("w: "+this.game.width + " h: "+this.game.height, 20,80, "#000000", "20px Arial");
             // if(this.hoverManager){
@@ -70,10 +92,10 @@ module GTE {
             // }
             // if(this.treeController.treeView.iSets){
             //     this.game.debug.text(this.treeController.tree.iSets.length+" "+this.treeController.treeView.iSets.length, 20,40, "#ff0000", "20px Arial");
-                // this.treeController.treeView.iSets.forEach(i=>{
-                //    this.game.debug.spriteBounds(i.label,"#00ff00",false);
-                //    this.game.debug.spriteBounds(i.ownerLabel,"#ff0000",false);
-                // });
+            // this.treeController.treeView.iSets.forEach(i=>{
+            //    this.game.debug.spriteBounds(i.label,"#00ff00",false);
+            //    this.game.debug.spriteBounds(i.ownerLabel,"#ff0000",false);
+            // });
             // }
             // if(this.userActionController.undoRedoController){
             //     this.game.debug.text(this.userActionController.undoRedoController.treesList.length+" "+this.userActionController.undoRedoController.treeCoordinates.length, 20,40, "#ff0000", "20px Arial");
